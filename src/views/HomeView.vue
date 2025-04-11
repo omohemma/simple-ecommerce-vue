aece0d9f6f6b4fd2f5a80e4ab1051ff9c67c37d7
Emmanuel Omololu <emmaomoh@gmail.com>
2025-04-11 09:15:14 +0100

    chore: category collection mobile-responsiveness

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,9 @@
     <!--    Category Collection-->
     <section class="bg-gray-100">
       <CategoryCollection :categories="categories">
-        <div class="flex items-center justify-between text-sm text-blue-700 font-semibold">
+        <div
+          class="flex flex-wrap items-center justify-between text-sm text-blue-700 font-semibold"
+        >
           <h2 class="text-2xl font-bold text-gray-900">Shop by Category</h2>
           <a class="" href="#"> Browse all categories</a>
         </div>
