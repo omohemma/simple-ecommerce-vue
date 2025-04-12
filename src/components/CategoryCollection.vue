7efea16cd0631f9a8d8a4b3755926505d833b92e
Emmanuel Omololu <emmaomoh@gmail.com>
2025-04-12 07:06:12 +0100

    chore: reduced category collections' spacing on larger viewport

--- src/components/CategoryCollection.vue
+++ src/components/CategoryCollection.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-    <div class="mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none lg:py-32">
+    <div class="mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none">
       <slot></slot>
 
       <div class="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0">
