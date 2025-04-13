efd95749dd6ba122a58118db9c052ef72d828452
Emmanuel Omololu <emmaomoh@gmail.com>
2025-04-13 17:50:55 +0100

    chore: added spacing to category and placeholder cards

--- src/components/CategoryCollection.vue
+++ src/components/CategoryCollection.vue
@@ -3,7 +3,7 @@
     <div class="mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none">
       <slot></slot>
 
-      <div class="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0">
+      <div class="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6">
         <CategoryItem
           v-for="{ name, image, slug } in categories"
           :key="name"
--- src/components/PlaceholderContainer.vue
+++ src/components/PlaceholderContainer.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
     <div class="mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none">
-      <div class="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0">
+      <div class="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6">
         <slot></slot>
       </div>
     </div>
